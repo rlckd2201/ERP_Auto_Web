414b1f1146d80941c5a62383a09731d10f3fe1d3
rlckd2201 <rlckd2201@users.noreply.github.com>
2026-08-14 10:39:19 +0900

    Deploy WEHAGO Win32 save dialog fix

--- web_v1/deploy/deploy_wehago_hotfix_20260814.ps1
+++ web_v1/deploy/deploy_wehago_hotfix_20260814.ps1
@@ -2,11 +2,11 @@ $ErrorActionPreference = "Stop"
 
 $resultPath = "C:\Doc_center\wehago_deploy_result.txt"
 $expectedVersion = "1.0.228"
-$sourceCommit = "24fe500"
+$sourceCommit = "ed2e724"
 $sourceBase = "https://raw.githubusercontent.com/rlckd2201/ERP_Auto_Web/$sourceCommit"
 $expectedHashes = @{
     "tax_crawler\base_handler.py" = "22b195b2ba9edbba058a37834d179987f18c92dd636e7267ab97ad9bc85fe660"
-    "tax_crawler\portal_wehago.py" = "21f42a51749f59539fd326fae8785fb176e7606bc83ac0ff326b7e5558917e95"
+    "tax_crawler\portal_wehago.py" = "9fb945d160b9c24b5e28a69acdbcaa614d7f2a35e95d8003101ded369f976887"
 }
 
 function Stop-WebBackend {
